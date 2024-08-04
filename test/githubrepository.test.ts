d575795d30494a93eb72667a1d6cce9546581626
iplaza <ignacioandresplazam@gmail.com>
2024-08-04 13:46:47 -0400

    Initial commit

---
+++ test/githubrepository.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Githubrepository from '../lib/githubrepository-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/githubrepository-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Githubrepository.GithubrepositoryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

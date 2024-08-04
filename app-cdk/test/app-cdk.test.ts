95db4bfd3ce2b600e4bd17bd1723cda3f4971516
iplaza <ignacioandresplazam@gmail.com>
2024-08-04 13:55:02 -0400

    Inicializa el proyecto CDK

--- app-cdk/test/app-cdk.test.ts
+++ app-cdk/test/app-cdk.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Githubrepository from '../lib/githubrepository-stack';
+// import * as AppCdk from '../lib/app-cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/githubrepository-stack.ts
+// example resource in lib/app-cdk-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Githubrepository.GithubrepositoryStack(app, 'MyTestStack');
+//   const stack = new AppCdk.AppCdkStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
